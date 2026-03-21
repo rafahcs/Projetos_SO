16677b987feabb22811df81f95c7107bbb50962e
Rafael Henrique <131375370+rafahcs@users.noreply.github.com>
2026-03-20 23:01:01 -0300

    Update wcat.c
    Refactor file processing to output to stdout and handle errors.
    wcat.c is pass tests 1, 2, 3, 4, 5. Version for final check.

--- wcat/wcat.c
+++ wcat/wcat.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void processa_arquivo(char* nome_f){
+int processa_arquivo(char* nome_f){
     FILE *f = fopen(nome_f, "r");
-    FILE *f2 = fopen("dest.txt", "a") ; //modo "a"(append) adiciona ao final a cada execução
 
     if(f == NULL){
-        printf("Erro ao abrir %s", nome_f);
+        fprintf(stdout,"wcat: cannot open file\n");
+        return 1; //Indica erro
     }
 
-    //leitura de 'f' e escrita em dest.txt
+    //leitura de 'f' e escrita em stdout(saída padrão)
     char buf[70];
     while(fgets(buf, sizeof buf, f) != NULL){
-        fputs(buf, f2);
+        fputs(buf, stdout); 
     }
 
     fclose(f);
+    return 0; //Sucesso
 }
 
 int main(int argc, char *argv[]){
+    int erro = 0;
     //entrada dos arquivos
     if(argc < 2){
-        printf("Uso: %s file1 file2 ...", argv[argc]);
-        return 1;
+        return 0;
     }
     //cat
     //loop começa em i=1 pois 0 é o nome do executado
     for(int i = 1; i < argc; i++){
         processa_arquivo(argv[i]);
+    
     }
+
+    return erro;
 }
